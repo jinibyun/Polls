ca24edef2682b9a67994994c0f88b976d41e535c
Jini Byun <jinibyun@gmail.com>
2024-11-12 20:23:02 +0000

    Enable checking answer to vote (still hardcode value)

--- src/app/polls/[id].js
+++ src/app/polls/[id].js
@@ -1,16 +1,64 @@
-import { useLocalSearchParams } from "expo-router";
-import React from "react";
-import { View, Text } from "react-native";
+import { Stack, useLocalSearchParams } from "expo-router";
+import React, {useState} from "react";
+import { View, Text, StyleSheet, Pressable, Button } from "react-native";
+import Feather from '@expo/vector-icons/Feather';
 
-export default function PollDetails(){
+const poll = {
+    question: 'React Native vs Flutter',
+    options: [
+        'React Native FTW',
+        'Flutter',
+        'SwiftUI'
+    ]
+}
+export default function PollDetails() {
     const { id } = useLocalSearchParams(); // please note file name [id].js  (this is a special way to pass parameter-->> dynamice pass parameter)
-    
+    const [selected, setSelected] = useState('React Native FTW');
+
+    const vote = () => {
+        // console.log('vote ' + selected);
+    }
+
     return (
-        <View>
-            <Text>
-                Poll Details : {id}
+        <View style={styles.container}>
+            <Stack.Screen options={{
+                title: 'Poll Voting'
+            }} />
+            <Text style={styles.question}>
+                {poll.question}
             </Text>
+            <View style={{gap:5}}>
+                {poll.options.map(option => (
+                    <Pressable 
+                        key={option} 
+                        style={styles.optionContainer}
+                        onPress={() => setSelected(option)}
+                    >
+                        <Feather name= {option === selected ? "check-circle": 'circle'} size={18} color={option === selected ? "green": 'gray'} />
+                        <Text>{option}</Text>
+                    </Pressable>
+                ))}
+            </View>
+            <Button title="Vote" onPress={vote}></Button>
         </View>
     );
 }
 
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+        gap:20
+    },
+    question: {
+        fontSize: 20,
+        fontWeight: '600',
+    },
+    optionContainer: {
+        backgroundColor: 'white',
+        padding: 10,
+        borderRadius : 5,
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10,
+    }
+});
