1262e6a2b8648a370b584ad8633f703946b18998
jinibyun <jinibyun@gmail.com>
2024-11-17 23:28:00 -0500

    user voting - NOTE: always check it out security on supabase for select, insert , upsert etc.

--- src/app/polls/[id].js
+++ src/app/polls/[id].js
@@ -4,12 +4,15 @@ import { View, Text, StyleSheet, Pressable, Button, Alert } from "react-native";
 import Feather from '@expo/vector-icons/Feather';
 import { ActivityIndicator } from "react-native";
 import { supabase } from "lib/supabase";
+import { useAuth } from "providers/AuthProvider";
 
 export default function PollDetails() {
     const { id } = useLocalSearchParams(); // please note file name [id].js  (this is a special way to pass parameter-->> dynamice pass parameter)
     const [selected, setSelected] = useState('');
     const [poll, setPoll] = useState(null);
+    const [userVote, setUserVote] = useState(null);
 
+    const { user } = useAuth();
     useEffect(() => {
         console.log(id);
 		const fetcthPolls = async () => {
@@ -26,11 +29,55 @@ export default function PollDetails() {
 			setPoll(data);
 		};
 
+        // fetch user's vote
+        const fetchUserVote = async() => {
+            let { data, error } = await supabase
+				.from('vote')
+				.select('*')
+                .eq('poll_id', +id)
+                .eq('user_id', user.id)
+                //.limit(1)
+                .single();
+			// if (error) {
+			// 	Alert.alert(error + ' -- Error fetching user vote');
+			// 	//console.log(error);
+			// }
+            //console.log(data)
+			setUserVote(data);
+            if(data){
+                setSelected(data.option);
+            }
+        }
+
+        fetchUserVote();
 		fetcthPolls();
 	}, []);
     
-    const vote = () => {
-        // console.log('vote ' + selected);
+    const vote = async () => {
+        // ref: https://supabase.com/dashboard/project/atlcphyotrpwzdvkomqj/editor/32791?schema=public
+        const newVote ={
+            option: selected,
+            poll_id: poll.id,
+            user_id: user.id
+        }
+        if(userVote){
+            newVote.id = userVote.id;
+        }
+
+        const { data, error } = await supabase
+        .from('vote')
+        // .insert([
+        // { option: selected, poll_id: poll.id, user_id: user.id },
+        // ])
+        .upsert([newVote])
+        .select().single();
+
+        if(error){
+            Alert.alert('Failed to vote');
+        } else{
+            setUserVote(data);
+            Alert.alert('Thank you for your vote');
+        }
     }
 
     if(!poll){
